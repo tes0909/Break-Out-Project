16e5cffe773a392404281333b80984cc05876661
BaekSeungWoo617 <koke100@naver.com>
2024-10-16 12:01:14 +0900

    Sound

--- BreakOutProject/Assets/Scripts/SoundManager.cs
+++ BreakOutProject/Assets/Scripts/SoundManager.cs
@@ -1,47 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
 
     [Header("Audio Sources")]
-    public AudioSource bgmSource; // 배경음용 AudioSource
-    public AudioSource sfxSource; // 효과음용 AudioSource
+    private AudioSource BGMSource; // 배경음용 AudioSource
+    private AudioSource SFXSource; // 효과음용 AudioSource
+    public AudioClip[] BGM_Clips; // 각 씬에 맞는 배경음 클립 배열
+    public AudioClip[] SFX_Clips; // 각 씬에 맞는 배경음 클립 배열
 
     private void Awake()
     {
-        if(Instance !=null && Instance !=this)//첫번째는 싱글톤이고 두번째는 중복생성 방지용
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
             return;
         }
+
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        // AudioSource 컴포넌트 추가
+        BGMSource = gameObject.AddComponent<AudioSource>();
+        SFXSource = gameObject.AddComponent<AudioSource>();
+    }
+    public void PlayBGM(AudioClip clip)
+    {
+        if (BGMSource.isPlaying)
+            BGMSource.Stop();
+
+        BGMSource.clip = clip;
+        BGMSource.loop = true; // 배경음은 반복 재생
+        BGMSource.Play();
     }
 
-    public void PlayBGM(AudioClip audioClip)
+    public void PlaySFX(AudioClip clip)
     {
-        if (bgmSource.isPlaying)
-            bgmSource.Stop();
-
-        bgmSource.clip = audioClip;
-        bgmSource.loop = true; // 배경음은 반복 재생
-        bgmSource.Play();
+        SFXSource.PlayOneShot(clip); // 효과음 재생
     }
-    public void PlaySFX(AudioClip audioClip)
+    private void OnEnable()
     {
-        sfxSource.PlayOneShot(audioClip); // 효과음은 한 번만 재생
+        SceneManager.sceneLoaded += OnSceneLoaded; // 씬이 로드될 때 호출될 메서드 등록
     }
 
-    public void SetBGMVolume(float volume)
+    private void OnDisable()
     {
-        bgmSource.volume = volume;
+        SceneManager.sceneLoaded -= OnSceneLoaded; // 씬 로드 시 등록 해제
     }
-
-    public void SetSFXVolume(float volume)
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        sfxSource.volume = volume;
+        PlayBGMForCurrentScene(); // 씬이 로드될 때 배경음 재생
+    }
+    private void PlayBGMForCurrentScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (sceneIndex < BGM_Clips.Length && BGM_Clips[sceneIndex] != null)
+        {
+            PlayBGM(BGM_Clips[sceneIndex]);
+        }
     }
 }
