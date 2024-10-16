b44041791717b54cba745b64570fb5ec4c3cadf6
1suyb <suyb1234@gmail.com>
2024-10-16 12:01:27 +0900

    update: IngameScene

--- BreakOutProject/Assets/Scripts/Scene/InGameScene.cs
+++ BreakOutProject/Assets/Scripts/Scene/InGameScene.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class InGameScene : BaseScene
 {
+	[SerializeField] private Vector3 _paddleInitPosition = new Vector3(0, -3, 0);
 	protected override void Awake()
 	{
 		base.Awake();
 		UIManager.Instance.OpenSceneUI("GameSceneUI");
+		GameObject paddle = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Paddle"));
+		paddle.transform.position = _paddleInitPosition;
 	}
 
 	protected override void Close()
