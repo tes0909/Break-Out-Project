44bf8f66703ecd20f168c7c75b8dfcc9fb508e52
1suyb <suyb1234@gmail.com>
2024-10-17 20:21:42 +0900

    update : sound연결 및 ui

--- BreakOutProject/Assets/Scripts/Sounds/SFXSoundVolume.cs
+++ BreakOutProject/Assets/Scripts/Sounds/SFXSoundVolume.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SFXSoundVolume : MonoBehaviour
+public interface IVolume
+{
+    public void UpdateSoundVolume(float volume);
+}
+public class SFXSoundVolume : MonoBehaviour, IVolume
 {
     AudioSource audioSource;
     [Range(0f, 1f)]
     public float AudioVolume;
     private void Start()
     {
-        AudioVolume = PlayerPrefs.GetFloat("Volume", 1f); // 저장된 볼륨 가져오기
+        AudioVolume = PlayerPrefs.GetFloat("SFXVolume", 1f); // 저장된 볼륨 가져오기
         audioSource = GetComponent<AudioSource>();
         if(SoundManager.SoundInstance!= null)
         {
-            SFXSlideChanged(SoundManager.SoundInstance.SFXSize);
+			UpdateSoundVolume(AudioVolume);
         }
     }
-    public void SFXSlideChanged(float value)
-    {
-        AudioVolume = value; // 슬라이더 값으로 AudioVolume 업데이트
-        PlayerPrefs.SetFloat("Volume", AudioVolume); // 볼륨 저장
-        audioSource.volume = AudioVolume; // AudioSource의 볼륨 설정
-        SoundManager.SoundInstance.SFXSize = value;
-        Debug.Log(SoundManager.SoundInstance.SFXSize);
-    }
+
+	public void UpdateSoundVolume(float volume)
+	{
+		AudioVolume = volume; // 슬라이더 값으로 AudioVolume 업데이트
+		PlayerPrefs.SetFloat("SFXVolume", AudioVolume); // 볼륨 저장
+		audioSource.volume = AudioVolume; // AudioSource의 볼륨 설정
+		Debug.Log(volume);
+	}
 }
--- BreakOutProject/Assets/Scripts/UI/Popup/UI_Menu.cs
+++ BreakOutProject/Assets/Scripts/UI/Popup/UI_Menu.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,49 @@ public class UI_Menu : UI_Popup
 {
 	[SerializeField] private Button _background;
 	[SerializeField] private Button _gameQuitButton;
+	[SerializeField] private Slider _bgmSlider;
+	[SerializeField] private Slider _sfxSlider;
+	[SerializeField] private Button _bgmOnOffButton;
+	[SerializeField] private Image _bgmOnImage;
+	[SerializeField] private Button _sfxOnOffButton;
+	[SerializeField] private Image _sfxOnImage;
+	[SerializeField] private Transform _bgmSelectLayout;
+
 
 	public void Awake()
 	{
 		_background.onClick.AddListener(CloseThisPopUp);
 		_gameQuitButton.onClick.AddListener(OpenGameQuitUI);
+		_bgmSlider.onValueChanged.AddListener(SoundManager.SoundInstance.SetBGMVolume);
+		_sfxSlider.onValueChanged.AddListener(SoundManager.SoundInstance.SetSFXVolume);
+		_bgmOnImage.gameObject.SetActive(SoundManager.SoundInstance.BGMOnOff);
+		_sfxOnImage.gameObject.SetActive(SoundManager.SoundInstance.SFXOnOff);
+		_bgmOnOffButton.onClick.AddListener(BGMOnOff);
+		_sfxOnOffButton.onClick.AddListener(SFXOnOff);
+		CreateSoundSelectButton();
 	}
-	
+
+	public void CreateSoundSelectButton()
+	{
+		int size = SoundManager.SoundInstance.BGM_Clips.Length;
+		for(int i = 0; i < size; i++)
+		{
+			UI_SubItem item = UIManager.Instance.CreateSubItemUI("SoundSelectButton", _bgmSelectLayout);
+			item.Init(i);
+		}
+	}
+	public void BGMOnOff()
+	{
+		SoundManager.SoundInstance.SetBGMOnOff();
+		_bgmOnImage.gameObject.SetActive(SoundManager.SoundInstance.BGMOnOff);
+	}
+	public void SFXOnOff()
+	{
+		SoundManager.SoundInstance.SetSFXOnOff();
+		_sfxOnImage.gameObject.SetActive(SoundManager.SoundInstance.SFXOnOff);
+
+	}
+
 	public void CloseThisPopUp()
 	{
 		UIManager.Instance.ClosePopUpUI("MenuUI");
