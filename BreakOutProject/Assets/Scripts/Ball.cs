e14ec22ee76db9df735a088c0538f34859076a26
BaekSeungWoo617 <koke100@naver.com>
2024-10-17 10:25:22 +0900

    Update Ball.cs

--- BreakOutProject/Assets/Scripts/Ball.cs
+++ BreakOutProject/Assets/Scripts/Ball.cs
@@ -42,7 +42,6 @@ public class Ball : MonoBehaviour
     //}
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         SoundManager.SoundInstance.PlaySFX(SoundManager.SoundInstance.SFX_Clips[1]);
         BallRigidbody = GetComponent<Rigidbody2D>();
         Vector2 bounceDirection = collision.contacts[0].normal;
