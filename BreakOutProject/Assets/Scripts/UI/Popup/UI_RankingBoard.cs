b31a4690ffc587f9b6345204668a9c477f1a8fec
1suyb <suyb1234@gmail.com>
2024-10-17 09:03:41 +0900

    feat: add ranking UI

--- BreakOutProject/Assets/Scripts/UI/Popup/UI_RankingBoard.cs
+++ BreakOutProject/Assets/Scripts/UI/Popup/UI_RankingBoard.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_RankingBoard : MonoBehaviour
+public class UI_RankingBoard : UI_Popup
 {
 	[SerializeField] private Button _background;
-	[SerializeField] private Transform _RankingLayout;
+	[SerializeField] private Transform _rankingLayout;
 
 	public void Awake()
 	{
@@ -16,6 +16,12 @@ public class UI_RankingBoard : MonoBehaviour
 	}
 	public void CreateRanking()
 	{
+		int rankCount = ScoreboardManager.Instance.Scores.Count;
+		for(int i = 0; i < rankCount; i++)
+		{
+			UI_SubItem ranking = UIManager.Instance.CreateSubItemUI("Ranking", _rankingLayout);
+			ranking.Init(i);
+		}
 	}
 
 	private void CloseThisPopUP()
