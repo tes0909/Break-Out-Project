32b73261f130a172320e00d7cd89bcba4222f5ee
BaekSeungWoo617 <koke100@naver.com>
2024-10-18 16:13:39 +0900

    Revert "Merge branch 'Dev' into 10_18)BallPrefabsSet(BSW)"
    This reverts commit 239785180af1c22e18ce9e5dd0f9e4e8ebdd2877, reversing
    changes made to 496b095aef10e1625b7796d5d429e5dbd2878b20.

--- BreakOutProject/Assets/Scripts/Ball.cs
+++ BreakOutProject/Assets/Scripts/Ball.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private Rigidbody2D BallRigidbody;// ???? BallRigidbody.velocity
-    [SerializeField] private int InputDirection;//?÷???? ??? ??????? ??
+    private Rigidbody2D BallRigidbody;// 공의 BallRigidbody.velocity
+    [SerializeField] private int InputDirection;//플레이어가 키를 입력했을 때
     //[SerializeField] private BallStat ballStat;
     public BallMoveSO ballMoveSO;
     public BallPowerSO ballPowerSO;
-    private float BallSpeed; //???? ?????
-    private float BallRotation;//???? ??? ??
-    PlayerInputController inputController;
+    private float BallSpeed; //공의 스피드
+    private float BallRotation;//공의 회전 값
+    InputManager inputManager;
 
     protected BallStatHandler ballStats { get; private set; }
     void Awake()
@@ -24,21 +24,20 @@ public class Ball : MonoBehaviour
 
     void Start()
     {
-        inputController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInputController>();
-        BallRigidbody = GetComponent<Rigidbody2D>();
+        inputManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InputManager>();
         BallRigidbody.velocity = new Vector2(Random.Range(-1f, 1f) * BallSpeed, BallSpeed);
         Game.Instance.GameManager.OnGameOver += DestroyThis;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //SoundManager.SoundInstance.PlaySFX(SoundManager.SoundInstance.SFX_Clips[1]);
-
+        // Game.Instance.SoundManager.PlaySFX(Game.Instance.SoundManager.SFX_Clips[1]); ResourceManager추가후 추가
+        BallRigidbody = GetComponent<Rigidbody2D>();
         Vector2 bounceDirection = collision.contacts[0].normal;
         
         BallRigidbody.AddForce(bounceDirection * BallSpeed, ForceMode2D.Impulse);
 
 
-        if (collision.collider.CompareTag("Player"))//?÷???? ???????
+        if (collision.collider.CompareTag("Player"))//플레이어 네임테그
         {
         ForceTheBall(Vector2.up, BallSpeed, BallRotation);
         }
@@ -63,35 +62,35 @@ public class Ball : MonoBehaviour
     {
         direction.Normalize();
         float BallRotationEX = BallRotation;
-        switch (inputController.moveInput)
+        switch (inputManager.horizontal)
         {
             case -1f:
-                BallRotationEX = BallRotation;//?÷???? ????? ???????? ????? ??
+                BallRotationEX = BallRotation;//플레이어 핸들러가 왼쪽으로 이동할 때
                 break;
             case 0f:
                 int zeroRadom = Random.Range(-1, 2);
-                BallRotationEX = (BallRotation * zeroRadom);//??????? -1,0,1?? ????? ??
+                BallRotationEX = (BallRotation * zeroRadom);//회전값이 -1,0,1중 하나가 됨
                 break;
             case 1f:
-                BallRotationEX = (-1f) * BallRotation;//?÷???? ????? ?????????? ????? ??
+                BallRotationEX = (-1f) * BallRotation;//플레이어 핸들러가 오른쪽으로 이동할 때
                 break;
             default:
                 break;
         }
 
-        // ?浹 ?? ???? ?????? ????????.
-        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) *direction; // ???? ???? ??????? ????
-        BallRigidbody.velocity = bounceDirection * BallSpeed; // ??? ???????? ??? ????
+        // 충돌 시 튕기는 방향을 수정합니다.
+        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) *direction; // 위쪽 방향에 회전값을 적용
+        BallRigidbody.velocity = bounceDirection * BallSpeed; // 회전 방향으로 속도 설정
     }
-    private void AddBallForce(Vector2 direction, float BallSpeed)//???? ????????.
+    private void AddBallForce(Vector2 direction, float BallSpeed)//힘을 더해줍니다.
     {
         float BallRotationEX = BallRotation;
         int zeroRadom = Random.Range(-1, 2);
-        BallRotationEX = (BallRotation * zeroRadom);//??????? -1,0,1?? ????? ??
+        BallRotationEX = (BallRotation * zeroRadom);//회전값이 -1,0,1중 하나가 됨
 
-        // ?浹 ?? ???? ?????? ????????.
-        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) * direction; // ???? ???? ??????? ????
-        BallRigidbody.velocity += bounceDirection * BallSpeed; // ??? ???????? ??? ????
+        // 충돌 시 튕기는 방향을 수정합니다.
+        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) * direction; // 위쪽 방향에 회전값을 적용
+        BallRigidbody.velocity += bounceDirection * BallSpeed; // 회전 방향으로 속도 설정
     }
     public void DestroyThis()
     {
---
+++ BreakOutProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InputManager : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [Range(0f, 30f)] [SerializeField] private float speed;
+
+    private const float LeftBoundary = -8.1f;
+    private const float RightBoundary = 8.1f;
+    public float horizontal;
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PaddleMovement();
+        ConstrainPosition();
+    }
+
+    void PaddleMovement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+       
+        Vector2 direction = new Vector2(horizontal, 0);
+        direction = direction.normalized;
+
+        rb.velocity = direction * speed;
+    }
+
+    void ConstrainPosition()
+    {
+        Vector2 position = transform.position;
+
+        if (position.x < LeftBoundary)
+            position.x = LeftBoundary;
+        else if (position.x > RightBoundary)
+            position.x = RightBoundary;
+
+        transform.position = position; 
+    }
+}
