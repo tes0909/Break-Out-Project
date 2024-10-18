4423a48bf753c068344c6a2a3a637708bbf63283
bc5546 <bc12345653@gmail.com>
2024-10-18 10:05:26 +0900

    [Refactor] integrate brickPool, itemPool

---
+++ BreakOutProject/Assets/GiHyeok/Script/PoolManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField] private GameObject brickPrefab;
+    [SerializeField] public Dictionary<string,Queue<GameObject>> totalPool;
+    public int maxbrick;
+
+    [SerializeField] private GameObject itemPrefab;
+    public int maxItem;
+    private void Awake()
+    {
+        Queue<GameObject> brickPool = new Queue<GameObject>();
+        for (int i = 0; i < maxbrick; i++)
+        {
+            GameObject obj = Instantiate(brickPrefab);
+            obj.SetActive(false);
+            brickPool.Enqueue(obj);
+        }
+
+        Queue<GameObject> itemPool = new Queue<GameObject>();
+        for (int i = 0; i < maxItem; i++)
+        {
+            GameObject obj = Instantiate(itemPrefab);
+            obj.SetActive(false);
+            itemPool.Enqueue(obj);
+        }
+        totalPool= new Dictionary<string,Queue<GameObject>>();
+        totalPool.Add("brick", brickPool);
+        totalPool.Add("item", itemPool);
+    }
+
+    public GameObject GetBrick()
+    {
+        GameObject obj = totalPool["brick"].Dequeue();
+        totalPool["brick"].Enqueue(obj);
+        return obj;
+    }
+
+    public GameObject GetItem()
+    {
+        GameObject obj = totalPool["item"].Dequeue();
+        totalPool["item"].Enqueue(obj);
+        return obj;
+    }
+
+    internal void ResetBrick()
+    {
+        foreach (GameObject obj in totalPool["brick"])
+        {
+            obj.SetActive(false);
+        }
+    }
+}
