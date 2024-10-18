c90d7422332693bf496464c1da430cf73c3769fa
BaekSeungWoo617 <koke100@naver.com>
2024-10-18 12:02:54 +0900

    BallMoveUpdate

--- BreakOutProject/Assets/Scripts/Ball.cs
+++ BreakOutProject/Assets/Scripts/Ball.cs
@@ -6,45 +6,56 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody2D BallRigidbody;// 공의 BallRigidbody.velocity
     [SerializeField] private int InputDirection;//플레이어가 키를 입력했을 때
-    [SerializeField] private BallStat ballStat;
+    //[SerializeField] private BallStat ballStat;
+    public BallMoveSO ballMoveSO;
     private float BallSpeed; //공의 스피드
     private float BallRotation;//공의 회전 값
-
     InputManager inputManager;
 
     protected BallStatHandler ballStats { get; private set; }
     void Awake()
     {
         ballStats = GetComponent<BallStatHandler>();
-        BallSpeed = ballStats.CurrentStat.currentSpeed;//BallSpeed = 10f;
-        BallRotation = ballStats.CurrentStat.currentRotate;//BallRotation = 40f;
+        BallRigidbody = GetComponent<Rigidbody2D>();
+        BallSpeed = ballMoveSO.changeSpeed;//BallSpeed = 10f;
+        BallRotation = ballMoveSO.changeRotate;//BallRotation = 40f;
     }
 
     void Start()
     {
         inputManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InputManager>();
-        Rigidbody2D BallRigidbody = GetComponent<Rigidbody2D>();
         BallRigidbody.velocity = new Vector2(Random.Range(-1f, 1f) * BallSpeed, BallSpeed);
     }
  
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    {     
+
         SoundManager.SoundInstance.PlaySFX(SoundManager.SoundInstance.SFX_Clips[1]);
-        BallRigidbody = GetComponent<Rigidbody2D>();
         Vector2 bounceDirection = collision.contacts[0].normal;
+        
         BallRigidbody.AddForce(bounceDirection * BallSpeed, ForceMode2D.Impulse);
+
 
         if (collision.collider.CompareTag("Player"))//플레이어 네임테그
         {
-            RotateArm();
+        ForceTheBall(Vector2.up, BallSpeed, BallRotation);
         }
         if (collision.collider.CompareTag("Brick"))
         {
             collision.gameObject.GetComponent<Brick>().GetDamage();
         }
+        if(collision.collider.CompareTag("SideWall"))
+        {
+            AddBallForce(Vector2.up, BallSpeed/2);
+        }
+        if (BallRigidbody.velocity.y == 0)
+        {
+            BallRigidbody.velocity = new Vector2(Random.Range(-1f, 1f) * BallSpeed, BallSpeed);
+        }
     }
-    private void RotateArm()
+    private void ForceTheBall(Vector2 direction,float BallSpeed, float BallRotation)
     {
+        direction.Normalize();
         float BallRotationEX = BallRotation;
         switch (inputManager.horizontal)
         {
@@ -61,10 +72,19 @@ public class Ball : MonoBehaviour
             default:
                 break;
         }
-        Debug.Log(BallRotationEX);
-        //this.transform.rotation = Quaternion.Euler(0, 0, BallRotationEX);
+
         // 충돌 시 튕기는 방향을 수정합니다.
-        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) * Vector2.up; // 위쪽 방향에 회전값을 적용
+        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) *direction; // 위쪽 방향에 회전값을 적용
         BallRigidbody.velocity = bounceDirection * BallSpeed; // 회전 방향으로 속도 설정
+    }
+    private void AddBallForce(Vector2 direction, float BallSpeed)//힘을 더해줍니다.
+    {
+        float BallRotationEX = BallRotation;
+        int zeroRadom = Random.Range(-1, 2);
+        BallRotationEX = (BallRotation * zeroRadom);//회전값이 -1,0,1중 하나가 됨
+
+        // 충돌 시 튕기는 방향을 수정합니다.
+        Vector2 bounceDirection = Quaternion.Euler(0, 0, BallRotationEX) * direction; // 위쪽 방향에 회전값을 적용
+        BallRigidbody.velocity += bounceDirection * BallSpeed; // 회전 방향으로 속도 설정
     }
 }
